bed9afe64b17aafa40f5cffca1cd829a88a3f708
dnbrd <33011795+dnbrd@users.noreply.github.com>
2017-10-24 23:57:16 -0700

    basic website template project

---
+++ Webpage.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<h1>I'm Dane<h1>
+<h1>My movie website</h1>
+<p>Welcome to my movie website where you can watch movies for free.</p>
+   </head>
+<body> 
+		<div id="menu-top">
+			<ul id="topnav">
+			<?php $link_name = "Home"; ?>
+				<li><a href="Home.php"><?php echo $link_name; ?>Home</a></li>
+			<?php $link_name = "Series"; ?>
+				<li><a href="Series.php"><?php echo $link_name; ?>Series</a></li>
+			<?php $link_name = "Newest_Episodes_Added"; ?>
+				<li><a href="Newest_Episodes_Added.php"><?php echo $link_name; ?>Newest_Episodes_Added</a></li>
+			<?php $link_name = "Week_Popular_Episodes"; ?>
+				<li><a href="Week_Popular_Episodes.php"><?php echo $link_name; ?>Week_Popular_Episodes</a></li>	
+			<?php $link_name = "TV_Schedule"; ?>
+				<li><a href="TV_Schedule.php"><?php echo $link_name; ?>TV_Schedule</a></li>
+			<?php $link_name = "Contact"; ?>
+				<li><a href="Contact.php"><?php echo $link_name; ?>Contact</a></li>	
+			<?php $link_name = "About_Us"; ?>
+				<li><a href="About_Us.php"><?php echo $link_name; ?>About_Us</a></li>	
+<body> 
+
+<html>
